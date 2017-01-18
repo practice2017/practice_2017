7b9157520d81d9dfd68c5dd05bccc2d5bc1805fe
knnas <knnasya@bk.ru>
2017-01-18 17:16:37 +0500

    First Commit

--- src/main/java/CassandraConnection.java
+++ src/main/java/CassandraConnection.java
@@ -1,6 +1,4 @@
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
 
 /**
  * Created by knnas on 18.01.2017.
@@ -11,25 +9,9 @@ public class CassandraConnection {
 
     public static void insertDataToBD(InitialData dt, Session session)
     {
-        String cmdInsert = "INSERT INTO practice_2017.practicetable" +
-                "(pspaynum, account, errcode, errtext, flags, indate, instatus, parentpayid, payamount, paytype, servcode, servtype, termid, totalpayamount)" +
-                "VALUES('"
-                + dt.psPayNum +
-                "', '" + dt.account +
-                "', '" + dt.errCode +
-                "', '" + dt.errText +
-                "', '" + dt.flags +
-                "', '" + dt.inDate +
-                "', '" + dt.inStatus +
-                "', '" + dt.parentPayId +
-                "', '" + dt.payAmount +
-                "', '" + dt.payType +
-                "', '" + dt.servCode +
-                "', '" + dt.servType +
-                "', '" + dt.termId +
-                "', '" + dt.totalPayAmount +
-                "')";
-        session.execute(cmdInsert);
+        PreparedStatement prepared = session.prepare("INSERT INTO practice_2017.practicetable (pspaynum, account, errcode, errtext, flags, indate, instatus, parentpayid, payamount, paytype, servcode, servtype, termid, totalpayamount) values (?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?)");
+        session.execute(prepared.bind(dt.psPayNum, dt.account,dt.errCode,dt.errText,dt.flags,dt.inDate,
+                     dt.inStatus,dt.parentPayId,dt.payAmount,dt.payType,dt.servCode,dt.servType,dt.termId,dt.totalPayAmount));
     }
 
 }
